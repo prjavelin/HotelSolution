224a409ec44a04ac3c391ec529efc52b7c16499f
prjavelin <prjavelin@gmail.com>
2023-01-16 19:43:21 -0400

    Add project files.

---
+++ Model/BlazorRoomcs.cs
@@ -0,0 +1,12 @@
+﻿namespace Hotel_v2.Model
+{
+    public class BlazorRoom
+    {
+        public int Id { get; set; }
+        public string RoomName { get; set; }
+
+        public double Price { get; set; }   
+
+        public bool IsActive { get; set; }
+    }
+}
