e45a30164f0498fd59c71d4ee3e752b8c5750d5f
prjavelin <prjavelin@gmail.com>
2023-01-22 15:48:43 -0400

    Added rooms and props

---
+++ Model/BlazorRoomProp.cs
@@ -0,0 +1,9 @@
+﻿namespace Hotel_v2.Model
+{
+    public class BlazorRoomProp
+    {
+        public int Id { get; set; } 
+        public string Name { get; set; }    
+        public string Value { get; set; }
+    }
+}
--- Model/BlazorRoomcs.cs
+++ Model/BlazorRoomcs.cs
@@ -8,5 +8,8 @@
         public double Price { get; set; }   
 
         public bool IsActive { get; set; }
+
+        public List<BlazorRoomProp> RoomProps { get; set; }
+
     }
 }
